871a1d7e4ec0c4f85e8bdfaf1cd07b10dd36ca72
MilSoltani <miladsoltani@outlook.de>
2026-03-09 18:18:09 +0100

    feat: add ticket schema

---
+++ backend/src/database/ticket.schema.ts
@@ -0,0 +1,30 @@
+import { integer, pgEnum, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod';
+
+export const statusEnum = pgEnum('status', ['open', 'pending', 'working', 'resolved', 'closed']);
+
+export const priorityEnum = pgEnum('priority', ['low', 'medium', 'high', 'urgent']);
+
+export const ticketsTable = pgTable('tickets', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  subject: varchar({ length: 512 }).notNull(),
+  status: statusEnum().notNull().default('open'),
+  priority: priorityEnum().notNull().default('low'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const ticketSchema = createSelectSchema(ticketsTable);
+
+export const ticketInsertSchema = createInsertSchema(ticketsTable)
+  .omit({
+    createdAt: true,
+    updatedAt: true,
+  });
+
+export const ticketUpdateSchema = createUpdateSchema(ticketsTable)
+  .omit({
+    createdAt: true,
+    updatedAt: true,
+  })
+  .partial();
