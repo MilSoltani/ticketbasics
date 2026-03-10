7bb40d82db667b56326e15afd11eb0df4e54f788
MilSoltani <miladsoltani@outlook.de>
2026-03-10 07:27:47 +0100

    fix: making Ticket inferred type work in frontend
    frontend does not understand the alias @

--- backend/src/client/ticket.client.ts
+++ backend/src/client/ticket.client.ts
@@ -1,5 +1,10 @@
+import type z from 'zod';
+
 import { hc } from 'hono/client';
 
+import type { ticketSchema } from '../database/schema';
 import type ticketHandler from '../handler/ticket.handler';
 
 export const ticketsClient = hc<typeof ticketHandler>('http://localhost:3000/api/tickets');
+
+export type Ticket = z.infer<typeof ticketSchema>;
