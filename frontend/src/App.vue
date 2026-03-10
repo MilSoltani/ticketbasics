8be06044337c66da153d50b59d5a4a314cfb20bf
MilSoltani <miladsoltani@outlook.de>
2026-03-10 06:49:34 +0100

    feat: used shadcn table for tickets

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,15 @@
 import { ticketsClient } from '@ticketbasics/backend/client';
 import { onMounted, ref } from 'vue';
 
+import Table from './components/ui/table/Table.vue';
+import TableBody from './components/ui/table/TableBody.vue';
+import TableCaption from './components/ui/table/TableCaption.vue';
+import TableCell from './components/ui/table/TableCell.vue';
+import TableFooter from './components/ui/table/TableFooter.vue';
+import TableHead from './components/ui/table/TableHead.vue';
+import TableHeader from './components/ui/table/TableHeader.vue';
+import TableRow from './components/ui/table/TableRow.vue';
+
 const tickets = ref<any[]>([]);
 
 async function fetchTickets() {
@@ -19,14 +28,35 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-for="ticket in tickets" :key="ticket.id" class="ticket-card">
-    <h2>{{ ticket.subject }}</h2>
-    <p>{{ ticket.description }}</p>
-    <div class="ticket-meta">
-      <span class="badge" :class="`status-${ticket.status}`">{{ ticket.status }}</span>
-      <span class="badge" :class="`priority-${ticket.priority}`">{{ ticket.priority }}</span>
-    </div>
-  </div>
+  <Table>
+    <TableCaption>A list of tickets.</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Id</TableHead>
+        <TableHead>Subject</TableHead>
+        <TableHead>Priority</TableHead>
+        <TableHead>Status</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="ticket in tickets" :key="ticket.id">
+        <TableCell>{{ ticket.id }}</TableCell>
+        <TableCell>{{ ticket.subject }}</TableCell>
+        <TableCell>{{ ticket.priority }}</TableCell>
+        <TableCell>{{ ticket.status }}</TableCell>
+      </TableRow>
+    </TableBody>
+    <TableFooter>
+      <TableRow>
+        <TableCell colspan="3">
+          Total
+        </TableCell>
+        <TableCell class="text-right">
+          {{ tickets.length }}
+        </TableCell>
+      </TableRow>
+    </TableFooter>
+  </Table>
 </template>
 
 <style scoped>
