3d4a19fc03d6276210f5e6b1ee754edc9049eff2
MilSoltani <miladsoltani@outlook.de>
2026-03-11 16:41:03 +0100

    feat: modal for creating ticket

--- frontend/src/pages/tickets.page.vue
+++ frontend/src/pages/tickets.page.vue
@@ -5,7 +5,18 @@ import { Plus } from 'lucide-vue-next';
 import { ref } from 'vue';
 
 import Button from '@/components/ui/button/Button.vue';
-import Input from '@/components/ui/input/Input.vue';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import Select from '@/components/ui/select/Select.vue';
 import SelectContent from '@/components/ui/select/SelectContent.vue';
 import SelectItem from '@/components/ui/select/SelectItem.vue';
@@ -35,26 +46,61 @@ const newTicket = ref<TicketCreatePayload>({
 </script>
 
 <template>
-  <div class="mt-2 flex">
-    <Input v-model="newTicket.subject" type="text" placeholder="subject" />
+  <Dialog>
+    <form>
+      <DialogTrigger as-child>
+        <Button variant="outline">
+          <Plus :size="16" /> New Ticket
+        </Button>
+      </DialogTrigger>
+      <DialogContent class="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>New Ticket</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're
+            done.
+          </DialogDescription>
+        </DialogHeader>
+        <div class="grid gap-4">
+          <div class="grid gap-3">
+            <Label for="new-ticket-subject">Subject *</Label>
+            <Input id="new-ticket-subject" v-model="newTicket.subject" type="text" placeholder="subject" />
+          </div>
 
-    <Textarea v-model="newTicket.description" placeholder="description" />
+          <div class="grid gap-3">
+            <Label for="new-ticket-description">Description</Label>
+            <Textarea id="new-ticket-description" v-model="newTicket.description" placeholder="description" />
+          </div>
 
-    <Select v-model="newTicket.priority">
-      <SelectTrigger>
-        <SelectValue placeholder="Priority" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem v-for="priority in priorityList" :key="priority" :value="priority">
-          {{ priority }}
-        </SelectItem>
-      </SelectContent>
-    </Select>
-
-    <Button @click="() => useCreateMutate(newTicket)">
-      <Plus :size="16" /> Create
-    </Button>
-  </div>
+          <div class="grid gap-3">
+            <Label for="new-ticket-priority">Priority *</Label>
+            <Select id="new-ticket-priority" v-model="newTicket.priority">
+              <SelectTrigger>
+                <SelectValue placeholder="Priority" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem v-for="priority in priorityList" :key="priority" :value="priority">
+                  {{ priority }}
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose as-child>
+            <Button variant="outline">
+              Cancel
+            </Button>
+          </DialogClose>
+          <DialogClose as-child>
+            <Button type="submit" @click="() => useCreateMutate(newTicket)">
+              <Plus :size="16" /> Create
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </form>
+  </Dialog>
 
   <div class="mt-2">
     <div v-if="isLoading">
